0813689ab3e23418ea654ae4c814aa678bc6ed0b
bimashazaman <bimashazaman1234@gmail.com>
2022-11-02 11:44:43 +0600

    yessuyghghhgfgfgytujFGDFHGFHDGF

--- components/property/Filters.js
+++ components/property/Filters.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Price from "./Price";
+import Price from "./price";
 import "rsuite/dist/rsuite.min.css";
 import { Formik } from "formik";
 
--- pages/boat/index.js
+++ pages/boat/index.js
@@ -3,7 +3,7 @@
 
 import React from "react";
 import Search from "../../components/Search/Search";
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import ShowSubCategories from "../../components/Boat/ShowSubCategories";
 import Recently_published from "../../components/Boat/recently_published";
 
